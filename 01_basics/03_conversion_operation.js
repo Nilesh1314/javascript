91eed65c894d92d9b415e6fee8cc469d67e8356e
Nilesh1314 <nilesh.nadsoft@gmail.com>
2025-09-01 10:15:32 +0530

    comparison

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -31,5 +31,27 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ###############  OPERATIONS  #####################
+let value = 3
+let negValue = value
+// console.log(2+2)
+// console.log("2" + 2) // "22"
+// console.log("2" - 2) // 0
+// console.log("2" * 2) // 4
+// console.log("2" / 2) // 1
+// console.log(2 ** 3) // 8
+// console.log(6 % 4) // 2
+
+let str1 = "Hello"
+let str2 = " Nilesh"
+str3 = str1 + str2 // str1 = str1 + str2
+console.log(str1)
+
+console.log('1' + 2) // "12"
+console.log(1 + '2') // "12"
+console.log('1' + '2') // "12"
+console.log('1' + 2 + 2) // "122"
+console.log(1 + 2 + '2')
