401400a1021c7ae3b5885f64eedb1705096c10b8
Nilesh1314 <nilesh.nadsoft@gmail.com>
2025-09-08 11:59:26 +0530

    conflict

--- 08_projects/project.js
+++ 08_projects/project.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', function(e) {
     
     if(height !== '' || height < 0 || isNaN(height)) {
         result.innerHTML = `Please Gave a Valid Height ${height}`
-    } else if(weight !== '' || weight < 0 || isNaN(weight)) {
+    } else if(weight !== '' || weight < 0 || isNaN(weight) || true) {
         result.innerHTML = `Please Gave a Valid Weight ${weight}`
     } else {
         
